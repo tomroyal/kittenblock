17b6d691a2d99010f346306f0dcf293318d71674
Tom Royal <tom@tomroyal.com>
2018-09-08 14:45:53 +0100

    fix block url

--- kblock.js
+++ kblock.js
@@ -1,10 +1,10 @@
 
 function checkforDM (tabId, changeInfo, tab) {
     if (tab.url.indexOf('dailymail.co.uk') > -1) {
-        chrome.tabs.update(tabId, {url: "http://www.tomroyal.com/teaandkittens/blocked.php?chrome"});	  
+        chrome.tabs.update(tabId, {url: "http://www.teaandkittens.co.uk/blocked/"});	  
     };
     if (tab.url.indexOf('www.express.co.uk') > -1) {
-        chrome.tabs.update(tabId, {url: "http://www.tomroyal.com/teaandkittens/blocked.php?chrome"});	  
+        chrome.tabs.update(tabId, {url: "http://www.teaandkittens.co.uk/blocked/"});	  
     };
 };
 // Listen
